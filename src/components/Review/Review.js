c4b11ef058b664b58e8432129662d718916996bc
chamakkarmakar <chamakkarmakar.ck@gmail.com>
2022-04-04 17:51:51 +0600

    show reviews in review pg

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import './Review.css';
 
-const Review = () => {
+const Review = (props) => {
+    const { name, review, img, rating } = props.review;
     return (
-        <div>
-            
-        </div>
+        <Card className='card' style={{ width: '18rem' }}>
+            <div className='review-details'>
+                <Card.Img variant="top" src={img} />
+                <div>
+                    <Card.Title>{name}</Card.Title>
+                    <small>Rating: {rating}</small>
+                </div>
+            </div>
+            <Card.Body>
+                <Card.Text>
+                    {review}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+
     );
 };
 
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import 'react-bootstrap'
+import useReviews from '../../hooks/useReviews';
+import Review from '../Review/Review';
 import './Reviews.css';
 
 const Reviews = () => {
-    
+    const [reviews,setReviews] = useReviews();
     return (
-        <div>
+        <div className='w-75 mx-auto'>
             <h1>Review Page</h1>
+            <div className="review-card">
             {
-                reviews.map(review => 
-                    
+                reviews.map(review => <Review
+                key={review.id}
+                review={review}
+                ></Review> 
                  )
             }
+            </div>
 
         </div>
     );
